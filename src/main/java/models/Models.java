1263d7463d069a48b4380d2e97b3b6fdde1b4410
a1astor <alastor2005@mail.ru>
2021-05-30 17:19:59 +0300

    Add basic project structure, add dao class for models table

---
+++ src/main/java/models/Models.java
@@ -0,0 +1,30 @@
+package models;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "models")
+@Data
+public class Models {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "mark")
+    private String mark;
+
+    @Column(name = "generation")
+    private String generation;
+
+    @Column(name = "model")
+    private String model;
+
+}
