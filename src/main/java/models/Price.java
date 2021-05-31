a711147ff76d44619a8e119cdb2699b85d2b350e
a1astor <alastor2005@mail.ru>
2021-06-01 00:21:56 +0300

    add entities class for other table

---
+++ src/main/java/models/Price.java
@@ -0,0 +1,28 @@
+package models;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "price")
+public class Price {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int car_id;
+
+    @Column(name = "start_price")
+    private int start_price;
+
+    @Column(name = "profitability_coeff")
+    private int profitability_coeff;
+
+    @Column(name = "amortization_coeff")
+    private int amortization_coeff;
+}
