e33034731f82db5c1399a6d38fe3123f28ca37f7
Andrey Sergeenko <a.sergeenko@intechcore.com>
2021-08-24 15:42:24 +0300

    Create two modules.

--- web/src/main/java/com/pac/security/AuthenticationRequestDTO.java
+++ web/src/main/java/com/pac/security/AuthenticationRequestDTO.java
@@ -1,9 +1,14 @@
 package com.pac.security;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
 public class AuthenticationRequestDTO {
+    @Email
     private String email;
+    @NotNull
     private String password;
 }
