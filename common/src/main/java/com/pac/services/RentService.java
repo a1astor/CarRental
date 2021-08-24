80b4de73120e746f62e43961d54607c8edd3bf93
Andrey Sergeenko <a.sergeenko@intechcore.com>
2021-08-24 16:02:59 +0300

    Final version

--- common/src/main/java/com/pac/services/RentService.java
+++ common/src/main/java/com/pac/services/RentService.java
@@ -5,5 +5,5 @@ import java.util.Date;
 import com.pac.model.Contract;
 
 public interface RentService {
-    Contract rent(long carId, Date startDate, Date endDate);
+    Contract rent(long carId, Date startDate, Date endDate, long userId);
 }
